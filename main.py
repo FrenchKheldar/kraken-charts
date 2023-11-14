29a1d45bfbf3894b8f67e644ed3e658c9bc2b9c6
FrenchKheldar <matthieu.masquelet@gmail.com>
2023-11-13 23:38:01 -0800

    Big progress, added basic goalie plots as well as flags, although flags still have some issues.

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def get_color_spectrum(n, color1, color2, number_bands=15):
 # Load citizenship data
 flags = pd.read_csv("flags.csv", header=0)
 
-# Load each season data
+
+# Load each season data for skaters
 data = []
 seasons = []
 for f in sorted(glob.glob("season*.csv")):
@@ -36,6 +37,7 @@ stacked_df = pd.concat(df.assign(Season = season) for df, season in zip(data, se
 
 # Adding additional stats
 stacked_df["PPP"] = stacked_df["PPG"] + stacked_df["PPA"]
+
 
 number_seasons = len(seasons)
 # Build list of unique players
@@ -74,6 +76,47 @@ for p in list_players:
 
 stacked_df["PlayerAndFlag"] = stacked_df["Player"] + stacked_df["Flag"]
 
+# Load each season data for goalies
+data = []
+seasons = []
+for f in sorted(glob.glob("goalies*.csv")):
+    seasons.append(Path(f).stem.split('goalies-')[-1])
+    print(f"Loading {f} for goalies...")
+    #data.append(pd.read_csv(f, header=0, skiprows=1, index_col=0, usecols=lambda x: x not in ['Rk']))
+    data.append(pd.read_csv(f, header=0, skiprows=1, usecols=lambda x: x not in ['Rk']))
+    #print(data[-1]["GWG"])
+stacked_dfg = pd.concat(df.assign(Season = season) for df, season in zip(data, seasons)).reset_index(drop=True)
+
+number_seasons = len(seasons)
+# Build list of unique goalies
+list_goalies = stacked_dfg.Player.unique().tolist()
+# For each player, add an entry for their career total
+for p in list_goalies:
+    # if the player has played only 1 season, we can't use sum
+    if stacked_dfg.loc[stacked_df.Player == p].shape[0] == 1:
+        total_row = stacked_dfg.loc[stacked_df["Player"] == p]
+        total_row["Season"] = "Total"
+        total_row["Age"] = 1
+        stacked_df = pd.concat([stacked_dfg, total_row]).reset_index(drop=True)
+    else:
+        total_row = stacked_dfg.loc[stacked_dfg["Player"] == p].sum()
+        total_row.loc["Player"] = p
+        total_row.loc["Season"] = "Total"
+        total_row.loc["Age"] = stacked_dfg.loc[stacked_dfg.Player == p].shape[0]
+        try:
+            total_row.loc["Flag"] = flags.loc[flags.Player == p].iloc[0]["Flag"]
+        except IndexError:
+            print("Missing flag",p)
+            total_row.loc["Flag"] = None
+        total_row.loc["url"] = stacked_dfg.loc[stacked_dfg.Player == p].iloc[0]["url"]
+        if total_row.loc["GA"] == 0:
+            total_row.loc["SV%"] = 100.
+        else:
+            total_row.loc["SV%"] = total_row.loc["SV"] / total_row.loc["SA"] * 100
+        stacked_dfg = pd.concat([stacked_dfg, total_row.to_frame().T], sort=True).reset_index(drop=True)
+
+stacked_dfg["PlayerAndFlag"] = stacked_dfg["Player"] + stacked_dfg["Flag"]
+
 
 def get_stat(df, p, s, stat):
     """
@@ -88,7 +131,7 @@ def get_stat(df, p, s, stat):
         return df[(df.Player == p) & (df.Season == s)][stat].values[0]
     except IndexError:
         return 0
-def plotAlltimeLeaders(stat, statName, num, singleSeasonRecord, team):
+def plotAlltimeLeaders(df, stat, statName, num, singleSeasonRecord, team, goalies=False):
     """
 
     :param stat: statistic to chart
@@ -98,7 +141,7 @@ def plotAlltimeLeaders(stat, statName, num, singleSeasonRecord, team):
     """
 
 
-    top_leaders = stacked_df.loc[stacked_df.Season == "Total"].sort_values(stat, ascending=False).iloc[:num][['Player', 'Flag', stat]].reset_index(drop=True)
+    top_leaders = df.loc[df.Season == "Total"].sort_values(stat, ascending=False).iloc[:num][['Player', 'Flag', stat]].reset_index(drop=True)
 
     # Basic plot
     # fig = go.Figure()
@@ -106,14 +149,14 @@ def plotAlltimeLeaders(stat, statName, num, singleSeasonRecord, team):
     # fig.write_image("top_goalscorers.png")
 
     # for p in top_goalscorers.Player:
-    #    print(p, [s for s in stacked_df.loc[stacked_df.Player == p].Season])
+    #    print(p, [s for s in df.loc[df.Player == p].Season])
 
     # Maybe I should think about setting a different index for the subset dfs
     fig = go.Figure()
-    names = [p.split()[-1] for p in top_leaders.Player]
-
+    names = [f"{p.split()[-1]} {f}" for p,f in zip(top_leaders.Player,top_leaders.Flag)]
+    print([n for n in names if "nan" in n])
     for n, s in enumerate(seasons):
-        stats = [get_stat(stacked_df, p, s, stat) for p in top_leaders.Player]
+        stats = [get_stat(df, p, s, stat) for p in top_leaders.Player]
         #print(stats[5])
         if s != 'Total':
             #colors = [get_color_spectrum(n,light_blue,dark_blue,number_seasons) for st in stats]
@@ -139,10 +182,18 @@ def plotAlltimeLeaders(stat, statName, num, singleSeasonRecord, team):
                          showlegend=True))
 
     fig.update_layout(barmode='stack')
+    fig.update_xaxes(tickangle=45)
     fig.update_layout(title_text=f"{team} All-Time Leaders in {statName}")
     #fig.show()
-    fig.write_image(f"top_leaders_{stat}_stacked.png",scale=2)
-    fig.write_html(f"top_leaders_{stat}_stacked.html")
+    if goalies:
+        fig.write_image(f"top_goalies_leaders_{stat}_stacked.png",scale=2)
+        fig.write_html(f"top_goalies_leaders_{stat}_stacked.html")
+    else:
+        fig.write_image(f"top_leaders_{stat}_stacked.png",scale=2)
+        fig.write_html(f"top_leaders_{stat}_stacked.html")
+
+
+
 team = "Seattle Kraken"
 statName = {"GP": "Games Played",
             "G": "Goals Scored",
@@ -164,7 +215,25 @@ statName = {"GP": "Games Played",
 single_season_records = {}
 for stat in ["GP", "G", "A", "PTS", "PIM", "EVG", "PPG", "SHG", "GWG", "EVA", "PPA", "PPP", "SHA", "S", "BLK", "HIT"]:
     single_season_records[stat] = stacked_df[stacked_df.Season != 'Total'][stat].max()
-    plotAlltimeLeaders(stat, statName[stat], 15, single_season_records[stat], team)
+    plotAlltimeLeaders(stacked_df, stat, statName[stat], 15, single_season_records[stat], team)
+
+single_season_records = {}
+statName = {"GP": "Games Played",
+            "GS": "Games Started",
+            "W": "Wins",
+            "L": "Losses",
+            "GA": "Goals Against",
+            "SA": "Shots Against",
+            "SV": "Saves",
+            "SO": "Shutouts",
+            "QS": "Quality Starts",
+            "GPS": "Goalie Point Shares",
+            }
+
+for stat in statName:
+    single_season_records[stat] = stacked_dfg[stacked_dfg.Season != 'Total'][stat].max()
+    plotAlltimeLeaders(stacked_dfg, stat, statName[stat], 15, single_season_records[stat], team, goalies=True)
+
 # TODO
 ## Use better flags like https://github.com/hampusborgos/country-flags
 ## or https://github.com/google/region-flags
